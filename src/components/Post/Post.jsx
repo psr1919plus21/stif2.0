4cb1f37364bc00799240754d0526b1da971dc6b3
psr1919plus21 <psr1919plus21@gmail.com>
2019-06-19 21:46:32 +0300

    :repeat: Refactor.

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -3,35 +3,6 @@ import PropTypes from 'prop-types';
 
 import './Post.scss';
 
-class Post extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            title: props.title,
-            description: props.description,
-            imgUrl: props.imgUrl,
-            url: props.url
-        };
-    }
-
-    render() {
-        return (
-            <article className="post">
-                <div className="post__content">
-                    <a className="post__link" href={this.props.url} target="_blank">
-                        <header className="post__header">
-                            <h2 className="post__title">{this.state.title}</h2>
-                        </header>
-                        <img src={this.state.imgUrl} alt={this.state.title} className="post__img"/>
-                        <p className="post__description">{this.state.description}</p>
-                    </a>
-                </div>
-            </article>
-        );
-    }
-}
-
 Post.propTypes = {
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
@@ -39,5 +10,21 @@ Post.propTypes = {
     url: PropTypes.string.isRequired,
 };
 
+function Post(props) {
+    return (
+        <article className="post">
+            <div className="post__content">
+                <a className="post__link" href={props.url} target="_blank" rel="noopener noreferrer">
+                    <header className="post__header">
+                        <h2 className="post__title">{props.title}</h2>
+                    </header>
+                    <img src={props.imgUrl} alt={props.title} className="post__img"/>
+                    <p className="post__description">{props.description}</p>
+                </a>
+            </div>
+        </article>
+    );
+
+}
 
 export default Post;
